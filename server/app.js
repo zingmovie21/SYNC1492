e0a21e7a212a85edfef6992f52cebaab2925d51a
zingmovie21 <133083254+zingmovie21@users.noreply.github.com>
2024-11-30 10:50:00 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,8 +7,10 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+const PORT = process.env.PORT || 3000;
 const rooms = {};
 
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
@@ -60,6 +62,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
